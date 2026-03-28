20b91dd91ef27d2000f2362bbeb2f1a7c7906c38
devasish-das <114371786+devasish-das@users.noreply.github.com>
2026-03-28 05:06:18 -0700

    Php,mysql and CSS has been used to create this mini app

---
+++ delete_page.php
@@ -0,0 +1,19 @@
+<?php include("db.php"); ?>
+
+<?php 
+
+if(isset($_GET['id']))
+    { 
+        $id=$_GET['id'];
+
+        $query= "delete from `students` where `id` = '$id'";
+         $result = mysqli_query($connection, $query);
+
+    if (!$result) {
+        die("query failed: " . mysqli_error($connection));
+    } else {
+        header('location:index.php?delete_msg=You have deleted the record.');
+    }
+    }
+
+?>
